3f1e552064a870d499946a31364cb774ce96e71b
Enrique <enrique_fernandez92@hotmail.com>
2019-06-08 19:04:13 -0700

    new athlete added

--- db/athletes.js
+++ db/athletes.js
@@ -28,6 +28,25 @@ let data = [
                 lvl: "lvl_rx"
             }
         ]
+    },  {
+        id: 2,
+        name: "Humberto",
+        lastName: "Felix",
+        skills: [
+            {
+                id: 1,
+                lvl: "lvl_sc"
+            },{
+                id: 2,
+                lvl: "lvl_rx"
+            },{
+                id: 3,
+                lvl: "lvl_rx"
+            },{
+                id: 4,
+                lvl: "lvl_rx"
+            }
+        ]
     }
 ]
 
